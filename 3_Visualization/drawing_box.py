0dc5403609895611b7dd2530d250b7e99424a83a
HyunCello <jeremykim95@gmail.com>
2022-04-24 23:28:05 +0900

    add images

--- 3_Visualization/drawing_box.py
+++ 3_Visualization/drawing_box.py
@@ -6,26 +6,25 @@ from PIL import ImageDraw
 np.set_printoptions(precision=2, suppress=True)
 
 
-
 def seprate(data):
     # data.split(' ')
     # print(data.split(' ')[2])
     seprated = []
-    for i in range(0,5):
+    for i in range(0, 5):
         if i == 4:
-            data_splited = data.split(' ')[i]
-            data_remove = data_splited.split('\n')[0]
+            data_splited = data.split(" ")[i]
+            data_remove = data_splited.split("\n")[0]
             seprated.append(data_remove)
         else:
-            seprated.append(data.split(' ')[i])
+            seprated.append(data.split(" ")[i])
     return seprated
 
 
 # print(img_name)
-img = Image.open("./img/000000.png")
+img = Image.open("./img/000025.png")
 draw = ImageDraw.Draw(img)
 
-f_read = open("result_train.txt",'r')
+f_read = open("../4_Result/210509/1_seg_proj_nolim_25.txt", "r")
 # f_write = open("result_train.txt",'w')
 
 lines = f_read.readlines()
@@ -40,41 +39,46 @@ for line in lines:
     if num > num_prev:
         if num == 0:
             img.save("./output/000000.png")
-            img_name = "./img/00000"+ str(num) +".png"
+            img_name = "./img/00000" + str(num) + ".png"
             img = Image.open(img_name)
             draw = ImageDraw.Draw(img)
         else:
             if num < 10:
-                save_path = "./output/00000"+ str(num) +".png"
+                save_path = "./output/00000" + str(num) + ".png"
                 img.save(save_path)
-                img_name = "./img/00000"+ str(num) +".png"
+                img_name = "./img/00000" + str(num) + ".png"
                 img = Image.open(img_name)
                 draw = ImageDraw.Draw(img)
             elif num < 100:
-                save_path = "./output/0000"+ str(num) +".png"
+                save_path = "./output/0000" + str(num) + ".png"
                 img.save(save_path)
-                img_name = "./img/0000"+ str(num) +".png"
+                img_name = "./img/0000" + str(num) + ".png"
                 img = Image.open(img_name)
                 draw = ImageDraw.Draw(img)
+                print("HI")
             elif num < 1000:
-                save_path = "./output/000"+ str(num) +".png"
+                save_path = "./output/000" + str(num) + ".png"
                 img.save(save_path)
-                img_name = "./img/000"+ str(num) +".png"
+                img_name = "./img/000" + str(num) + ".png"
                 img = Image.open(img_name)
                 draw = ImageDraw.Draw(img)
             else:
-                save_path = "./output/00"+ str(num) +".png"
+                save_path = "./output/00" + str(num) + ".png"
                 img.save(save_path)
-                img_name = "./img/00"+ str(num) +".png"
+                img_name = "./img/00" + str(num) + ".png"
                 img = Image.open(img_name)
                 draw = ImageDraw.Draw(img)
         print(num_prev)
         num_prev = num
         draw.rectangle(((x1, y1), (x2, y2)), outline=(0, 0, 255), width=4)
+        # draw.rectangle((0, 0, 200, 200), outline=(0, 0, 255), width=4)
 
     else:
         draw.rectangle(((x1, y1), (x2, y2)), outline=(0, 0, 255), width=4)
     # if int(line_seprated[0])>= 2000:
     #     break
+print(save_path)
+img.save(save_path)
+
 
 f_read.close
